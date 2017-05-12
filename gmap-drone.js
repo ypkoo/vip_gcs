9f46932871d2fb20216bf3c6a5c88eddc2600d4d
David Hostallero <ddhostallero@gmail.com>
2017-05-12 11:55:09 +0900

    Replace drone list

--- gmap-drone.js
+++ gmap-drone.js
@@ -1,143 +1,98 @@
 var map;
-var startLatLng_ADD = {lat: 36.426060, lng: 127.309237}
-var startLatLng = {lat: 36.374092, lng: 127.365638}
-var startLatLng2 = {lat: 36.374383, lng: 127.365327}
-var startLatLng_DJI = {lat: 22.542813, lng: 113.958902}
-var marker;
-var droneList = [];
-var lineList = [];
 var gcsMarker;
+var drone_dict = {};
+var lineList = [];
+
 
 var lineSymbol = {
 	path: google.maps.SymbolPath.FORWARD_CLOSED_ARROW
 };
 
-function struct_drone() {
-	var marker = '';
-	var id = '';
-	var infoWindow = '';
-}
-
-
-
+// Initialize the map. Called when this file is loaded.
 function initMap() {
+	var GCSLatLng = {lat: 36.374092, lng: 127.365638};
+
 	map = new google.maps.Map(document.getElementById('map'), {
-		center: startLatLng,
+		center: GCSLatLng,
 		zoom: 17,
-		// mapTypeId: google.maps.MapTypeId.HYBRID,
 		mapTypeId: google.maps.MapTypeId.SATELLITE,
-		disableDefaultUI: true,
+		disableDefaultUI: true
 	});
 
-	// var image = 'images/drone.png';
 	map.addListener('click', map_clicked);
 
-	// test marker
+	//Locations of the GCS
 	gcsMarker = new google.maps.Marker({
-		position: startLatLng,
-		icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png',
-		// label: 'G'
+		position: GCSLatLng,
+		icon: 'http://maps.google.com/mapfiles/ms/icons/green-dot.png'
 	});
-
-
-
-	var test_marker = new google.maps.Marker({
-		position: startLatLng2,
-		// icon: 'http://maps.google.com/mapfiles/ms/icons/red-dot.png',
-		map: map,
-		label: '1'
-	});
-
-
-	test_marker.addListener('click', function() {
-
-		markerPos = test_marker.getPosition()
-		gcsPos = gcsMarker.getPosition()
-
-		if (gcsMarker.getMap() != null) {
-			dist = google.maps.geometry.spherical.computeDistanceBetween (markerPos, gcsPos);
-		}
-		else {
-			dist = "no_gcs_position";
-		}
-
-		jsCommunicator.emit_signal("marker_click_event " + '1' + " " + dist);
-	});
-
 }
 
+// map listener
 function map_clicked(e) {
 	var latLng = e.latLng;
 	var lat = latLng.lat();
 	var lng = latLng.lng();
 
+	//console.log("map_click_event " + lat + " " + lng);
 	jsCommunicator.emit_signal("map_click_event " + lat + " " + lng);
 }
 
 
+// updates the position of the marker
+// creates a new marker if the id is not recognized
 function update_marker(id, lat, lng) {
 
-	var idx = droneList.length;
-	// existing drone
-	for(var i=0; i<idx; i++) {
-		if (id == droneList[i].id) {
-			droneList[i].marker.setPosition({lat: lat, lng: lng});
-			droneList[i].marker.setMap(map);
-			// droneList[i].infoWindow.setContent(infoString);
-			return;
-		}
+	if (id in drone_dict) {
+		var marker = drone_dict[id];
+		marker.setPosition({lat: lat, lng: lng});
+	} 
+	else {
+		var marker = new google.maps.Marker({
+			position: {lat: lat, lng: lng},
+			label: id + "a",
+			map: map
+		});
+
+		marker.addListener('click', function() {
+			markerPos = marker.getPosition()
+			gcsPos = gcsMarker.getPosition()
+
+			if (gcsMarker.getMap() != null) {
+				dist = google.maps.geometry.spherical
+					.computeDistanceBetween(markerPos, gcsPos);
+			}
+			else {
+				dist = "no_gcs_position";
+			}
+
+			// console.log("marker_click_event " + id + " " + dist);
+			jsCommunicator.emit_signal(("marker_click_event " + id + " " + dist);
+		});
+
+		drone_dict[id] = marker;
 	}
-	// new drone
-	var marker = new google.maps.Marker({
-		position: {lat: lat, lng: lng},
-		label: id + "a",
-		map: map
-	});
-
-
-
-	marker.addListener('click', function() {
-		markerPos = marker.getPosition()
-		gcsPos = gcsMarker.getPosition()
-
-		if (gcsMarker.getMap() != null) {
-			dist = google.maps.geometry.spherical.computeDistanceBetween(markerPos, gcsPos);
-		}
-		else {
-			dist = "no_gcs_position";
-		}
-		
-
-		jsCommunicator.emit_signal("marker_click_event " + id + " " + dist);
-	});
-
-	droneList[idx] = new struct_drone();
-	droneList[idx].marker = marker;
-	droneList[idx].id = id;
-	// droneList[idx].infoWindow = infoWindow;
 }
 
 
+// removes marker from the map
 function remove_marker(id) {
-
-	var idx = droneList.length;
-	for(var i=0; i<idx; i++) {
-		if (id == droneList[i].id) {
-			droneList[i].marker.setMap(null);
-			droneList[i].id = -1;
-			break;
-		}
+	if (id in drone_dict) {
+		drone_dict[id].setMap(null);
+		drone_dict[id] = null;
+		// do not delete the ID so that it can be recycled
+		// also deleting will hurt performance
 	}
 }
 
+// removes all markers from the map
 function remove_all_markers() {
-	var idx = droneList.length;
-	for (var i=0; i<idx; i++) {
-		droneList[i].marker.setMap(null);
+	for (var id in drone_dict) {
+		drone_dict[id].setMap(null);
 	}
-
-	markerList = [];
+	drone_dict = {};
 }
+
 
 function draw_line(startLat, startLng, endLat, endLng) {
 	var line = new google.maps.Polyline({
@@ -185,5 +140,6 @@ function mark_gcs_position(lat, lng) {
 }
 
 function distance(lat1, lng1, lat2, lng2) {
-	return google.maps.geometry.spherical.computeDistanceBetween ({lat: lat1, lng: lng1}, {lat: lat2, lng: lng2});
+	return google.maps.geometry.spherical.computeDistanceBetween(
+		{lat: lat1, lng: lng1}, {lat: lat2, lng: lng2});
 }
