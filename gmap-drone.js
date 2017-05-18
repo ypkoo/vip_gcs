94be8cf67f8866f913abbda79cd81d359ebfc6fa
David Hostallero <ddhostallero@gmail.com>
2017-05-18 22:39:44 +0900

    Debug update drones

--- gmap-drone.js
+++ gmap-drone.js
@@ -49,7 +49,7 @@ function map_clicked(e) {
 // creates a new marker if the id is not recognized
 function update_marker(id, lat, lng) {
 
-	if (id in drone_dict) {
+	if (id in drone_dict && drone_dict[id] != null) {
 		var marker = drone_dict[id];
 		marker.setPosition({lat: lat, lng: lng});
 	} 
